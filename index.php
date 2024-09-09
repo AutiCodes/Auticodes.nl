733e528e281b59e7e9cc6dbe7d7970857908e457
KelvinCodes <31496522+AutiCodes@users.noreply.github.com>
2024-09-09 08:09:38 +0200

    Added meta info

--- index.php
+++ index.php
@@ -5,8 +5,9 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
   <title>AutiCodes</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
+  <meta content="My portfolio" name="description">
+  <meta content="Kelvin de Reus AutiCodes Portfolio" name="keywords">
+  <meta content="Kelvin de Reus" name="author">
 
   <!-- Favicons -->
   <link href="assets/img/favicon.ico" rel="icon">
